c09a60f8c329c8f00dd5466df0b0a772a54f0418
Tina <nguyennga.hcm@gmail.com>
2021-07-09 00:02:51 +0700

    New and crypto

--- crypto.js
+++ crypto.js
@@ -1,164 +1,20 @@
 const cryptoURL =
   "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=3&convert=USD&CMC_PRO_API_KEY=164359c0-aec7-4b9a-b562-783e4724fc85";
 
-//Note: change coin to bitcoin
-// const bitcoin = {
-//   id: 1,
-//   name: "Bitcoin",
-//   symbol: "BTC",
-//   slug: "bitcoin",
-//   num_market_pairs: 9852,
-//   date_added: "2013-04-28T00:00:00.000Z",
-//   tags: [
-//     "mineable",
-//     "pow",
-//     "sha-256",
-//     "store-of-value",
-//     "state-channels",
-//     "coinbase-ventures-portfolio",
-//     "three-arrows-capital-portfolio",
-//     "polychain-capital-portfolio",
-//   ],
-//   max_supply: 21000000,
-//   circulating_supply: 18651531,
-//   total_supply: 18651531,
-//   platform: null,
-//   cmc_rank: 1,
-//   last_updated: "2021-03-12T07:22:02.000Z",
-//   quote: {
-//     USD: {
-//       price: 56772.22509134523,
-//       volume_24h: 56146078171.69906,
-//       percent_change_1h: 0.45440337,
-//       percent_change_24h: 2.1565927,
-//       percent_change_7d: 20.01380942,
-//       percent_change_30d: 22.3897859,
-//       percent_change_60d: 67.00783825,
-//       percent_change_90d: 209.20232707,
-//       market_cap: 1058888916230.2034,
-//       last_updated: "2021-03-12T07:22:02.000Z",
-//     },
-//   },
-// };
-// // ethereum
-// const ethereum = {
-//   id: 1027,
-//   name: "Ethereum",
-//   symbol: "ETH",
-//   slug: "ethereum",
-//   num_market_pairs: 5918,
-//   date_added: "2015-08-07T00:00:00.000Z",
-//   tags: [
-//     "mineable",
-//     "pow",
-//     "smart-contracts",
-//     "ethereum",
-//     "coinbase-ventures-portfolio",
-//     "three-arrows-capital-portfolio",
-//     "polychain-capital-portfolio",
-//     "binance-labs-portfolio",
-//     "arrington-xrp-capital",
-//     "blockchain-capital-portfolio",
-//     "boostvc-portfolio",
-//     "cms-holdings-portfolio",
-//     "dcg-portfolio",
-//     "dragonfly-capital-portfolio",
-//     "electric-capital-portfolio",
-//     "fabric-ventures-portfolio",
-//     "framework-ventures",
-//     "hashkey-capital-portfolio",
-//     "kinetic-capital",
-//     "huobi-capital",
-//     "alameda-research-portfolio",
-//     "a16z-portfolio",
-//     "1confirmation-portfolio",
-//     "winklevoss-capital",
-//     "usv-portfolio",
-//     "placeholder-ventures-portfolio",
-//     "pantera-capital-portfolio",
-//     "multicoin-capital-portfolio",
-//     "paradigm-xzy-screener",
-//   ],
-//   max_supply: null,
-//   circulating_supply: 116605372.374,
-//   total_supply: 116605372.374,
-//   platform: null,
-//   cmc_rank: 2,
-//   last_updated: "2021-07-08T07:00:02.000Z",
-//   quote: {
-//     USD: {
-//       price: 2214.426752731016,
-//       volume_24h: 22060323132.90815,
-//       percent_change_1h: -0.0990944,
-//       percent_change_24h: -7.08782029,
-//       percent_change_7d: 2.38003753,
-//       percent_change_30d: -10.56826397,
-//       percent_change_60d: -44.10982227,
-//       percent_change_90d: 6.61681778,
-//       market_cap: 258214056097.14777,
-//       last_updated: "2021-07-08T07:00:02.000Z",
-//     },
-//   },
-// };
-// // Tether
-// const Tether = {
-//   id: 825,
-//   name: "Tether",
-//   symbol: "USDT",
-//   slug: "tether",
-//   num_market_pairs: 13764,
-//   date_added: "2015-02-25T00:00:00.000Z",
-//   tags: [
-//     "payments",
-//     "stablecoin",
-//     "stablecoin-asset-backed",
-//     "solana-ecosystem",
-//   ],
-//   max_supply: null,
-//   circulating_supply: 62301822762.351685,
-//   total_supply: 64469767616.826355,
-//   platform: {
-//     id: 1027,
-//     name: "Ethereum",
-//     symbol: "ETH",
-//     slug: "ethereum",
-//     token_address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
-//   },
-//   cmc_rank: 3,
-//   last_updated: "2021-07-08T06:59:09.000Z",
-//   quote: {
-//     USD: {
-//       price: 1.00069966947423,
-//       volume_24h: 51665678683.13653,
-//       percent_change_1h: -0.0141263,
-//       percent_change_24h: 0.0594254,
-//       percent_change_7d: 0.02700259,
-//       percent_change_30d: -0.04283363,
-//       percent_change_60d: 0.08920704,
-//       percent_change_90d: -0.05255862,
-//       market_cap: 62345413445.92739,
-//       last_updated: "2021-07-08T06:59:09.000Z",
-//     },
-//   },
-// };
-// var ctx = document.getElementById("myChart");
-// var ctx = document.getElementById("myChart").getContext("2d");
-// var ctx = $("#myChart");
-// var ctx = "myChart";
-// var ctx = document.getElementById("myChart");
 const totalCoin = ["Bitcoin", "Ethereum", "Tether"];
 // 5. Consume Data into Chart
 async function getCryptoPrices() {
   const response = await fetch(cryptoURL);
+  // console.log("response", response);
   const json = await response.json();
   // const Bitcoin = json.data[0];
-
+  // console.log("json", json);
   // renderLineGraph(Bitcoin);
   // renderLineGraph(json.data);
   // for (let index = 0; index < 3; index++) {
   //   // const element = array[index];
-  //   const coins = json.data[index];
-  //   renderLineGraph(json.data[0],json.data[1],json.data[2]);
+  //   // const coins = json.data[index];
+  //   renderLineGraph(json.data[index]);
   // }
   renderLineGraph(json.data[0], json.data[1], json.data[2]);
 }
@@ -166,16 +22,25 @@ getCryptoPrices();
 
 function renderLineGraph(coin1, coin2, coin3) {
   const ctx = document.getElementById("myChart");
+
   const price1 = coin1.quote.USD.price;
   const price2 = coin2.quote.USD.price;
   const price3 = coin3.quote.USD.price;
-  console.log("price", price1);
-  console.log("here", coin1);
+  // console.log("price", price1);
+  // console.log("coin", coin1);
   const [ninetyAgoPrice] = getHistoricPrices(coin1.quote.USD);
-  console.log("ninetyAgoPrice", ninetyAgoPrice);
-
+  // console.log("ninetyAgoPrice", ninetyAgoPrice);
+  const coinColor = {
+    Bitcoin: "rgba(255, 9, 132, 1)",
+    Ethereum: "rgba(25, 9, 132, 1)",
+    Tether: "rgba(235, 987, 3, 1)",
+  };
+  const coinBGColor = {
+    Bitcoin: "rgba(255, 9, 132, 0.2)",
+    Ethereum: "rgba(25, 9, 132, 0.2)",
+    Tether: "rgba(235, 987, 3, 0.2)",
+  };
   const timeAgo = ["90d", "60d", "30d", "7d", "24h", "1h", "Current"];
-
   const myChart = new Chart(ctx, {
     type: "line",
     data: {
@@ -183,25 +48,25 @@ function renderLineGraph(coin1, coin2, coin3) {
       // labels: ["Bitcoin", "Ethereum", "Tether"],
       datasets: [
         {
-          label: "Bitcoin",
+          label: coin1.name + " USD",
           borderWidth: 1,
           data: getHistoricPrices(coin1.quote.USD),
-          borderColor: "rgba(255, 9, 132, 1)",
-          backgroundColor: "rgba(255, 9, 132, 0.2)",
+          borderColor: coinColor[coin1.name],
+          backgroundColor: coinBGColor[coin1.name],
         },
         {
-          label: "Ethereum",
+          label: coin2.name + " USD",
           borderWidth: 1,
           data: getHistoricPrices(coin2.quote.USD),
-          borderColor: "rgba(25, 99, 132, 1)",
-          backgroundColor: "rgba(25, 99, 132, 0.2)",
+          borderColor: coinColor[coin2.name],
+          backgroundColor: coinBGColor[coin2.name],
         },
         {
-          label: "Tether",
+          label: coin3.name + " USD",
           borderWidth: 1,
           data: getHistoricPrices(coin3.quote.USD),
-          borderColor: "rgba(55, 99, 32, 1)",
-          backgroundColor: "rgba(55, 99, 132, 0.2)",
+          borderColor: coinColor[coin3.name],
+          backgroundColor: coinBGColor[coin3.name],
         },
       ],
     },
@@ -225,6 +90,7 @@ function renderLineGraph(coin1, coin2, coin3) {
   });
   // console.log("check", myChart);
 }
+
 function getHistoricPrices(prices) {
   const {
     percent_change_90d,
@@ -262,13 +128,13 @@ function getHistoricPrices(prices) {
   );
 
   return [
-    ninetyAgoPrice,
-    sixtyAgoPrice,
-    thirtyAgoPrice,
-    sevenAgoPrice,
-    dayAgoPrice,
-    hourAgoPrice,
-    price,
+    Math.floor(ninetyAgoPrice),
+    Math.floor(sixtyAgoPrice),
+    Math.floor(thirtyAgoPrice),
+    Math.floor(sevenAgoPrice),
+    Math.floor(dayAgoPrice),
+    Math.floor(hourAgoPrice),
+    Math.floor(price),
   ];
 }
 function calculatePriceFromPercentageChange(currentPrice, percentageChange) {
@@ -289,7 +155,10 @@ function calculatePriceFromPercentageChange(currentPrice, percentageChange) {
     const original = (currentPrice / (100 + percentageChange)) * 100;
     historicPrice = original;
   }
+  // let historicPrice = Math.floor(historicPrice);
   return historicPrice;
+  // console.log("price", historicPrice);
+  // console.log("priceType", typeof historicPrice);
 }
 // var myChart = new Chart(ctx, {
 //   type: "line",
---
+++ news.js
@@ -0,0 +1,49 @@
+function produceDefaultUrl() {
+  let url = `https://newsapi.org/v2/top-headlines?apiKey=e80cb128944d4bd683a8cd4b5d675d74`;
+
+  // Look at all url query parameters and add them to the url above to respect language/country/category/page/etc.
+  const urlParams = window.location.search.split("?")[1];
+  console.log("here", window);
+  // console.log("here", urlParams);
+  // Guard against no url params and default to english.
+  if (!urlParams) return url + "&language=en";
+
+  urlParams.split("&").map((p) => {
+    // "Massage data" into workable form.
+    const [key, value] = p.split("=");
+    url += `&${key}=${value}`;
+  });
+
+  return url;
+}
+let url = produceDefaultUrl();
+
+async function getArticles() {
+  const response = await fetch(url);
+  const json = await response.json();
+  const { articles } = json;
+  console.log({ json });
+  document.getElementById("title").innerHTML = `CoderNews (${articles.length})`;
+  const articlesHTML = articles.map(renderArticle);
+  console.log(articlesHTML);
+  document.getElementById("newsList").innerHTML = articlesHTML.join("");
+}
+
+getArticles();
+function renderArticle(article) {
+  if (article.urlToImage != undefined && article.author != undefined) {
+    return `
+    <li class="mb-3 align-self-center article">
+        ${article.title}
+        <img src="${article.urlToImage}" alt="Snow" width="900" 
+        height="500"/>
+      </div>
+        <i class="fa fa-edit fa-xs"></i><h4 class="mb-0">${article.author}</h4>
+        <p><i class="fa fa-envelope"></i>${article.publishedAt}</p>
+        <h6 class="mb-0"><a href="${article.url}">${article.source.name}</a></h6>
+      <p><i class="fa fa-envelope"></i>${article.content}</p>
+
+    </li>
+  `;
+  }
+}
