b409b327e58e8993edb465d9076eba68cf6ccbc8
Tina <nguyennga.hcm@gmail.com>
2021-07-09 16:03:39 +0700

    Test

--- language.js
+++ language.js
@@ -1,9 +1,9 @@
-const languages = ["ar", "zh", "en", "es", "fr", "ru"];
+// const languages = ["ar", "zh", "en", "es", "fr", "ru"];
 
-function renderLanguageAnchorTags() {
-  const languageHTML = languages.map(
-    (l) => `<a href="http://127.0.0.1:5500/Index.html?language=${l}">${l}</a>`
-  );
-  document.getElementById("language").innerHTML = languageHTML;
-}
-// renderLanguageAnchorTags();
+// function renderLanguageAnchorTags() {
+//   const languageHTML = languages.map(
+//     (l) => `<a href="http://127.0.0.1:5500/Index.html?language=${l}">${l}</a>`
+//   );
+//   document.getElementById("language").innerHTML = languageHTML;
+// }
+// // renderLanguageAnchorTags();
