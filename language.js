5e5a98f9e965474bfa5bf44ccf1593fd0e68d006
Tina <nguyennga.hcm@gmail.com>
2021-07-11 14:01:50 +0700

    Fix pathname

--- language.js
+++ language.js
@@ -2,8 +2,10 @@ const languages = ["ar", "zh", "en", "es", "fr", "ru"];
 
 function renderLanguageAnchorTags() {
   let result = window.location.origin;
+  let result2 = window.location.pathname;
+
   const languageHTML = languages.map(
-    (el) => `<a href="${result}/Index.html?lang=${el}">${el}</a>`
+    (el) => `<a href="${result}${result2}?lang=${el}">${el}</a>`
   );
   document.getElementById("language").innerHTML = languageHTML;
   // document.getElementById("languages").innerHTML = languagesMap.
