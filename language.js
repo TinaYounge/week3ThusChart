c178bbdfb75431e585d5fb316267ebe6d14f0879
Tina <nguyennga.hcm@gmail.com>
2021-07-11 10:10:59 +0700

    check URL

--- language.js
+++ language.js
@@ -1,9 +1,10 @@
-// const languages = ["ar", "zh", "en", "es", "fr", "ru"];
+const languages = ["ar", "zh", "en", "es", "fr", "ru"];
 
-// function renderLanguageAnchorTags() {
-//   const languageHTML = languages.map(
-//     (l) => `<a href="http://127.0.0.1:5500/Index.html?language=${l}">${l}</a>`
-//   );
-//   document.getElementById("language").innerHTML = languageHTML;
-// }
-// // renderLanguageAnchorTags();
+function renderLanguageAnchorTags() {
+  const languageHTML = languages.map(
+    (el) => `<a href="http://localhost:5500/Index.html?lang=${el}">${el}</a>`
+  );
+  document.getElementById("language").innerHTML = languageHTML;
+  // document.getElementById("languages").innerHTML = languagesMap.
+}
+renderLanguageAnchorTags();
--- news.js
+++ news.js
@@ -1,31 +1,43 @@
 function produceDefaultUrl() {
-  let url = `https://newsapi.org/v2/top-headlines?apiKey=e80cb128944d4bd683a8cd4b5d675d74`;
+  let url = `https://gnews.io/api/v4/top-headlines?token=bad48fad81cfc0564a7919cdc8e2f935
+  `;
 
-  // Look at all url query parameters and add them to the url above to respect language/country/category/page/etc.
   const urlParams = window.location.search.split("?")[1];
-  if (!urlParams) return url + "&language=en";
-  urlParams.split("&").map((p) => {
-    // "Massage data" into workable form.
-    const [key, value] = p.split("=");
-    url += `&${key}=${value}`;
-  });
+  // console.log(window);
+  console.log("check urlpara", urlParams);
+
+  if (!urlParams) {
+    return url + "&lang=en";
+  } else {
+    url += "&" + urlParams;
+    console.log("check urlpara", url);
+  }
+  // urlParams.split("&").map((p) => {
+  //   // "Massage data" into workable form.
+  //   const [key, value] = p.split("=");
+  //   url += `&${key}=${value}`;
+
+  // });
+  console.log("check url", url);
 
   return url;
 }
 let url = produceDefaultUrl();
+console.log("check url", url);
+// console.log("windown", window);
 
 async function getArticles() {
   const response = await fetch(url);
   const json = await response.json();
   const { articles } = json;
-  console.log({ json });
+  console.log("json form api", { json });
   const articlesHTML = articles.map(renderArticle);
   console.log(articlesHTML);
   document.getElementById("newsList").innerHTML = articlesHTML.join("");
   let goodArticles = articles.reduce((accumulator, currentValue) => {
     if (
-      currentValue.urlToImage != undefined &&
-      currentValue.author != undefined
+      currentValue.image != undefined &&
+      currentValue.source.name != undefined
     )
       return accumulator + 1;
     else return accumulator;
@@ -37,14 +49,14 @@ async function getArticles() {
 
 getArticles();
 function renderArticle(article) {
-  if (article.urlToImage != undefined && article.author != undefined) {
+  if (article.image != undefined && article.source.name != undefined) {
     return `
     <li class="mb-3 align-self-center article">
         ${article.title}
-        <img src="${article.urlToImage}" alt="Snow" width="900" 
+        <img src="${article.image}" alt="Snow" width="900" 
         height="500"/>
       </div>
-        <i class="fa fa-edit fa-xs"></i><h4 class="mb-0">${article.author}</h4>
+        <i class="fa fa-edit fa-xs"></i><h4 class="mb-0">${article.source.name}</h4>
         <p><i class="fa fa-envelope"></i>${article.publishedAt}</p>
         <h6 class="mb-0"><a href="${article.url}">${article.source.name}</a></h6>
       <p><i class="fa fa-envelope"></i>${article.content}</p>
